4b9049397fe722153ecb6b6a7fff548ead318936
hhw12409 <sksk2425@naver.com>
2022-09-11 02:16:37 +0900

    Docs : Tv Data Type narrowing

--- src/screens/Tv/Tv.tsx
+++ src/screens/Tv/Tv.tsx
@@ -1,29 +1,20 @@
-import { QueryClient, useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
-import { RefreshControl, ScrollView } from "react-native";
-import { tvApi } from "../../apis/apis";
-import HList from "../../components/HList/HList";
-import Loader from "../../components/Loader/Loader";
+import { QueryClient, useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
+import { tvApi } from '../../apis/apis';
+import HList from '../../components/HList/HList';
+import Loader from '../../components/Loader/Loader';
 
 const Tv = () => {
   const queryClinet = new QueryClient();
   const [refreshing, setRefreshing] = useState(false);
-  const { isLoading: todayLoading, data: todayData } = useQuery(
-    ["tv", "today"],
-    tvApi.getAiringToday
-  );
-  const { isLoading: topLoading, data: topData } = useQuery(
-    ["tv", "top"],
-    tvApi.getTopRated
-  );
-  const { isLoading: trendingLoading, data: trendingData } = useQuery(
-    ["tv", "trending"],
-    tvApi.getTrending
-  );
+  const { isLoading: todayLoading, data: todayData } = useQuery(['tv', 'today'], tvApi.getAiringToday);
+  const { isLoading: topLoading, data: topData } = useQuery(['tv', 'top'], tvApi.getTopRated);
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(['tv', 'trending'], tvApi.getTrending);
   const loading = todayLoading || topLoading || trendingLoading;
   const onRefresh = async () => {
     setRefreshing(true);
-    await queryClinet.refetchQueries(["tv"]);
+    await queryClinet.refetchQueries(['tv']);
     setRefreshing(false);
   };
   if (loading) {
@@ -32,13 +23,11 @@ const Tv = () => {
   return (
     <ScrollView
       contentContainerStyle={{ paddingVertical: 30 }}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
     >
-      <HList title="Trending TV" data={trendingData.results} />
-      <HList title="Airing Today" data={todayData.results} />
-      <HList title="Top Rated TV" data={topData.results} />
+      <HList title="Trending TV" data={trendingData!.results} />
+      <HList title="Airing Today" data={todayData!.results} />
+      <HList title="Top Rated TV" data={topData!.results} />
     </ScrollView>
   );
 };
